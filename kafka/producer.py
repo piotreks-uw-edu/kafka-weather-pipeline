94d2871bae3d623a577ae3c86da2a953a012435a
Piotr <piotr.slusarczyk@wat.edu.pl>
2024-03-18 16:07:00 +0100

    CutopProducer created

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,42 +1,49 @@
-import sys, os
+import os, sys
 from datetime import datetime
-from confluent_kafka import Producer
+from confluent_kafka import Producer as ConfluentProducer
 
-# Load environment variables for Kafka configuration
-bootstrap_servers = os.environ.get('BOOTSTAP_SERVERS')
-security_protocol = os.environ.get('SECURITY_PROTOCOL')
-sasl_mechanism = os.environ.get('SASL_MECHANISM')
-sasl_username = os.environ.get('SASL_USERNAME')
-client_id = os.environ.get('CLIENT_ID')
-shared_access_key = os.environ.get('SHARED_ACCESS_KEY')
-sasl_password=f"Endpoint=sb://{bootstrap_servers}/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey={shared_access_key}"
-topic = os.environ.get('TOPIC')
+class CustomProducer(ConfluentProducer):
+    def __init__(self):
+        self.topic = os.environ.get('TOPIC')
 
-if __name__ == "__main__":
-    conf = {
-        'bootstrap.servers' : bootstrap_servers,
-        'security.protocol' : security_protocol,
-        'sasl.mechanism' : sasl_mechanism,
-        'sasl.username' : sasl_username,
-        'sasl.password' : sasl_password,
-        'client.id' : client_id,
-    }
+        bootstrap_servers = os.environ.get('BOOTSTRAP_SERVERS')
+        security_protocol = os.environ.get('SECURITY_PROTOCOL')
+        sasl_mechanism = os.environ.get('SASL_MECHANISM')
+        sasl_username = os.environ.get('SASL_USERNAME')
+        client_id = os.environ.get('CLIENT_ID')
+        shared_access_key = os.environ.get('SHARED_ACCESS_KEY')
+        sasl_password = f"Endpoint=sb://{bootstrap_servers}/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey={shared_access_key}"
+        
 
-    # Instantiate the Kafka Producer with the configuration
-    p = Producer(**conf)
+        conf = {
+            'bootstrap.servers': bootstrap_servers,
+            'security.protocol': security_protocol,
+            'sasl.mechanism': sasl_mechanism,
+            'sasl.username': sasl_username,
+            'sasl.password': sasl_password,
+            'client.id': client_id,
+        }
+        super().__init__(**conf)
 
-    def deliver_callback(err, msg):
+
+    def deliver_callback(self, err, msg):
         if err:
             sys.stderr.write(f'Message failed delivery: {err}\n')
         else:
             sys.stderr.write(f'Message delivered to topic= {msg.topic()}, partition= [{msg.partition()}], offset= {msg.offset():o}\n')
+
+
+if __name__ == "__main__":
+
+    # Instantiate the Kafka Producer with the configuration
+    p = CustomProducer()
 
     # Produce messages to the Kafka topic
     for i in range(0,100):
         key = "even" if i%2 == 0 else "odd"
         try:
             message = str(i) + ' ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            p.produce(topic, message, callback=deliver_callback, key=key)
+            p.produce(p.topic, message, callback=p.deliver_callback, key=key)
         except BufferError as e:
             sys.stderr.write(f'Local Producer queue full ({len(p)} messages awaiting delivery) try again\n')
 
