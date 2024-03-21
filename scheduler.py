3d7387936ea3038460acfd286c07e4e8a0a2b319
Piotr <piotr.slusarczyk@wat.edu.pl>
2024-03-21 16:12:20 +0100

    Printing

--- scheduler.py
+++ scheduler.py
@@ -3,5 +3,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def schedule(scheduled_task):
     scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(scheduled_task, 'interval', minutes=10)
+    scheduler.add_job(scheduled_task, 'interval', minutes=1)
     scheduler.start()    
