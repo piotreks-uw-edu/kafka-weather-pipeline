0cf9ebf07b2aa155bd711ac3e13994c2fc9efde4
piotreks-uw-edu <>
2024-03-21 03:16:51 +0100

    Erro handling

--- application.py
+++ application.py
@@ -10,6 +10,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from collections import deque
 from datetime import datetime
 import random
+from requests.exceptions import SSLError
+import time
 
 app = Flask(__name__)
 
@@ -24,38 +26,46 @@ def scheduled_task():
     start_time = datetime.now()
     for latitude in np.arange(e.south_point, e.north_point, step):
         for longitude in np.arange(e.west_point, e.east_point, step):
-            pollution_data = get_pollution_data(latitude, longitude, api_key)
-            # location_data = get_location_data(latitude, longitude, api_key)
-            weather_data = get_weather_data(latitude, longitude, api_key)
             try:
-                country = weather_data['sys']['country']
-            except KeyError:
-                country = 'no country'
+                pollution_data = get_pollution_data(latitude, longitude, api_key)
+                # location_data = get_location_data(latitude, longitude, api_key)
+                weather_data = get_weather_data(latitude, longitude, api_key)
+                try:
+                    country = weather_data['sys']['country']
+                except KeyError:
+                    country = 'no country'
 
-            message_json = {
-                "pollution": pollution_data,
-                # "location": location_data,
-                "weather": weather_data
-            }
+                message_json = {
+                    "pollution": pollution_data,
+                    # "location": location_data,
+                    "weather": weather_data
+                }
 
-            message = json.dumps(message_json)
+                message = json.dumps(message_json)
+                print(country)
 
-            try:
-                producer.produce(producer.topic,
-                                 message,
-                                 callback=producer.deliver_callback,
-                                 key=country)
-            except BufferError:
-                sys.stderr.write(
-                    f'Local Producer queue full ({len(p)} messages awaiting delivery) try again\n')
+                try:
+                    producer.produce(producer.topic,
+                                    message,
+                                    callback=producer.deliver_callback,
+                                    key=country)
+                except BufferError:
+                    sys.stderr.write(
+                        f'Local Producer queue full ({len(p)} messages awaiting delivery) try again\n')
 
-            # The call will return immediately without blocking
-            producer.poll(0)
+                # The call will return immediately without blocking
+                producer.poll(0)
 
-            count += 1
+                count += 1
 
-            if count % 100:
-                producer.flush()
+                if count % 100:
+                    producer.flush()
+            except SSLError as ex:
+                print("An SSL error occurred:", ex)
+                time.sleep(30)
+            except Exception as ex:
+                print("An unexpected error occurred:", ex)
+                time.sleep(30)
 
     producer.flush()
     end_time = datetime.now()
@@ -69,6 +79,7 @@ scheduler = BackgroundScheduler(daemon=True)
 scheduler.add_job(scheduled_task, 'interval', hours=1)
 
 scheduler.start()
+scheduled_task()
 fifo_queue.append(f'Started at {datetime.now()}')
 
 
